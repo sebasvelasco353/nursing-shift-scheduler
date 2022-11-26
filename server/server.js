b52b8683650d0ada3729871db0a33b2143c780f9
Sebastian Velasco Borrero <11669998+sebasvelasco353@users.noreply.github.com>
2022-11-25 19:36:51 -0600

    completed basic server file, get nurses and shifts work, put shift wip

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 9001;
+
+const nurse_list = require('./nurse_list.json');
+const shift_list = require('./shift_list.json');
+
+app.use(express.json()); // Middlware to convert req body to json.
+
+app.get('/nurse', (req, res) => {
+    res.status(200).json(nurse_list);
+});
+app.get('/shifts', (req, res) => {
+    res.status(200).json(shift_list)
+});
+app.put('/shifts/:shift_id', (req, res) => {
+    console.log(req.params);
+    res.status(200).send({requestBody: req.body})
+});
+
+// Start the server.
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
